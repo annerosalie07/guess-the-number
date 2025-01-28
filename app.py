81834c289260fb22fde764dd46bdfcf75ec72163
Marta B <martaabednaruk@gmail.com>
2025-01-28 13:20:31 +0000

    Add requirements.txt and update app.py

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def guess():
     except ValueError:
         return "Please enter a valid number."
 if __name__ == "__main__":
-    app.run(debug=True)
+     app.run(host="0.0.0.0", port=5000, debug=True)
